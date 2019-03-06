b1c2d55c88fabfe6ecae581e3ba06a42bf78e655
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-03-06 11:56:07 -0800

    Created FETCH_SWAPI_START Reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios from "axios";
 
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
@@ -7,10 +7,11 @@ export const FETCH_SWAPI_START = "FETCH_SWAPI_START";
 export const FETCH_SWAPI_SUCCESS = "FETCH_SWAPI_SUCCESS";
 export const FETCH_SWAPI_FAILURE = "FETCH_SWAPI_FAILURE";
 
-export fetchSwapiPeople = () => dispatch => {
-    dispatch({ type: FETCH_SWAPI_START })
+export const fetchSwapiPeople = () => dispatch => {
+	dispatch({ type: FETCH_SWAPI_START });
 
-    axios.get('https://swapi.co/api/people/')
-        .then(res => dispatch({ type: FETCH_SWAPI_SUCCESS }))
-        .catch(err => dispatch({ type: FETCH_SWAPI_FAILURE })
-}
+	axios
+		.get("https://swapi.co/api/people/")
+		.then(res => dispatch({ type: FETCH_SWAPI_SUCCESS }))
+		.catch(err => dispatch({ type: FETCH_SWAPI_FAILURE }));
+};
--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,14 +1,24 @@
-import /* we need our action types here*/ "../actions";
+import {
+	FETCH_SWAPI_START,
+	FETCH_SWAPI_SUCCESS,
+	FETCH_SWAPI_FAILURE
+} from "../actions";
+
 const initialState = {
-  characters: []
-  // Array characters, Boolean fetching, null error.
+	characters: [],
+	fetching: false,
+	errors: null
 };
+
 export const charsReducer = (state = initialState, action) => {
-  switch (action.type) {
-    // Fill me in with the important reducers
-    // action types should be FETCHING, SUCCESS and FAILURE
-    // your switch statement should handle all of these cases.
-    default:
-      return state;
-  }
+	switch (action.type) {
+		case FETCH_SWAPI_START:
+			return {
+				...state,
+				fetching: true,
+				errors: null
+			};
+		default:
+			return state;
+	}
 };
